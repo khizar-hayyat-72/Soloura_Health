f1013620825c9f65b29e779bce70c28b4cf8219f
Syed Usama Hanif <106013156+Levinardo@users.noreply.github.com>
2025-07-09 11:49:41 +0500

    fixed mobile issues

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  server: {
+    url: 'http://192.168.18.150:3000', // URL of your Next.js app during development
+    cleartext: true, // Allow cleartext traffic for development
+  },
   appId: 'com.soloura.app',
   appName: 'Soloura',
   webDir: 'out', // Points to the static export directory
