9db94f9ec34b5474e2e03ac764ef506ddff388ca
Syed Usama Hanif <106013156+Levinardo@users.noreply.github.com>
2025-07-16 14:15:57 +0500

    fixed url issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+
+  server: {
+    url: 'https://solourahealth.netlify.app/dashboard', // URL of your Next.js app during development
+    // url: 'http://192.168.18.150:3000', // URL of your Next.js app during development
+    cleartext: true, // Allow cleartext traffic for development
+  },
+  ios: {
+    contentInset: 'always', // Adjusts the content inset for iOS
+    allowsLinkPreview: true, // Allows link previews in iOS
+  },
   appId: 'com.soloura.app',
   appName: 'Soloura',
   webDir: 'out',
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-// src/pages/_app.tsx
 import type { AppProps } from 'next/app';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className="safe-area">
       <Component {...pageProps} />
       <Toaster />
     </div>
