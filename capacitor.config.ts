6cedd2933f0c14c6f514f04c9a83380c884fd432
Syed Usama Hanif <106013156+Levinardo@users.noreply.github.com>
2025-07-22 16:05:11 +0500

    check for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   server: {
-    url: 'http://192.168.18.150:3000', // URL of your Next.js app during development
+    // url: 'https://solourahealth.netlify.app/login', // URL of your Next.js app during development
+    url: 'http://192.168.100.107:3000', // URL of your Next.js app during development
     cleartext: true, // Allow cleartext traffic for development
+  },
+  ios: {
+    contentInset: 'always', 
+    preferredContentMode: 'mobile',
+    // Adjusts the content inset for iOS
+    allowsLinkPreview: true, // Allows link previews in iOS
   },
   appId: 'com.soloura.app',
   appName: 'Soloura',
